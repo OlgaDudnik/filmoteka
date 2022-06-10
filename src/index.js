d86e8b9a66c285d30976b27ac5bce65a95a85f18
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-06-10 20:46:04 +0300

    добавила все файлы для группы

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './sass/main.scss';
+import './js/button';
+import './js/cards';
+import './js/header';
+import './js/loader';
+import './js/modal';
+import './js/modal_students';
+import './js/pagination';
+import './sass/utils/_variables.scss';
+import './sass/_common.scss';
+import './sass/components';
