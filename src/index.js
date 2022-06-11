455baf6edce15390448d29f4f00062820ab22d45
Андрій Шевельов <ashevelevmti@gmail.com>
2022-06-11 09:30:12 +0300

    add modal test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import './sass/main.scss';
 import './js/button';
 import './js/cards';
 import './js/header';
 import './js/loader';
-import './js/modal';
+import './sass/components/_modal.scss';
 import './js/modal_students';
 import './js/pagination';
 import './sass/utils/_variables.scss';
-import './sass/_common.scss';
-import './sass/components';
+// import './sass/_common.scss';
+// import './sass/components'
