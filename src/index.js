65eaa42be80caf457a3f54e6df4aec1b3bdbc24f
Андрій Шевельов <ashevelevmti@gmail.com>
2022-06-11 11:12:06 +0300

    add js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import './js/button';
 import './js/cards';
 import './js/header';
 import './js/loader';
-import './sass/components/_modal.scss';
+import './js/modal';
 import './js/modal_students';
 import './js/pagination';
 import './sass/utils/_variables.scss';
 // import './sass/_common.scss';
-// import './sass/components'
+import './sass/components/_modal.scss';
