18b438e2a5ce8c1adeef7f5fa5ca1f9022a861a2
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-06-12 21:57:06 +0300

    added library header

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  header: document.querySelector('.header'),
+  buttonHeaderHome: document.querySelector('#home'),
+  buttonHeaderLibrary: document.querySelector('#library'),
+  headerForm: document.querySelector('.header-form'),
+  buttonQueue: '',
+  buttonWatched: '',
+};
