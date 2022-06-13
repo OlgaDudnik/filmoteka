21c675a5f2be1fe77479b84896e53ffbd8c8370f
AlinaChurilova <aachcos8@gmail.com>
2022-06-13 22:05:17 +0300

    local storage functions added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,8 +1,18 @@
+const STORAGE_KEY1 = 'state-of-watched-movies';
+const STORAGE_KEY2 = 'state-of-queue';
 const backdrop = document.querySelector('.backdrop');
 const onCloseBtn = document.querySelector('.modal__button-close');
 const onModalBtn = document.querySelector('.onModalBtn');
 
+const watchedBtn = document.querySelector('#watched');
+const queueBtn = document.querySelector('#queue');
+
+
+watchedBtn.addEventListener('click', onAddToWatched);
+queueBtn.addEventListener('click', onAddToQueue);
+
 onModalBtn.addEventListener('click', onOpenModal);
+
 
 function onOpenModal(e) {
   console.log(e.target.src);
@@ -31,4 +41,38 @@ function modalCloseClickBackdrop(e) {
   if (e.target.nodeName === 'BACKDROP') {
     onCloseModal();
   }
+}
+
+const id = 1;
+
+function onAddToWatched() {
+  const watchedList = [];
+  const storageState = localStorage.getItem(STORAGE_KEY1);
+  
+  if (storageState) {
+  watchedList.push(...JSON.parse(storageState));
+  }
+  if (storageState?.includes(id)) {
+    
+    return;
+  }
+  watchedList.push(id);
+  const records = JSON.stringify(Object.values(watchedList));
+ return localStorage.setItem(STORAGE_KEY1, records);
+}
+
+function onAddToQueue() {
+  
+  const queueList = [];
+  const storageState = localStorage.getItem(STORAGE_KEY2);
+  
+  if (storageState) {
+  queueList.push(...JSON.parse(storageState));
+  }
+  if (storageState?.includes(id)) {
+    return;
+  }
+  queueList.push(id);
+  const records = JSON.stringify(Object.values(queueList));
+ return localStorage.setItem(STORAGE_KEY2, records);
 }
