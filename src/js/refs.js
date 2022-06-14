a3bc4f5c0e5de5c8a847192d3313f83a502dcdd3
Андрій Шевельов <ashevelevmti@gmail.com>
2022-06-14 21:23:24 +0300

    fix conflict

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,13 +1,17 @@
 export const refs = {
-
- //шапка
+  //шапка
   header: document.querySelector('.header'),
+  buttonHeaderHome: document.querySelector('#button-home'),
+  buttonHeaderLibrary: document.querySelector('#button-library'),
+  headerForm: document.querySelector('.header-form'),
+  buttonQueue: '',
+  buttonWatched: '',
+  collection: document.querySelector('.card__colection'),
   buttonHeaderHome: document.querySelector('#home'),
   buttonHeaderLibrary: document.querySelector('#library'),
   headerForm: document.querySelector('.header-form'),
   watchedBtn: document.querySelector('#watched'),
-    queueBtn: document.querySelector('#queue'),
-
+  queueBtn: document.querySelector('#queue'),
   //библиотека
   myLibraryContainer: document.querySelector('.library .container'),
 };
