56194d0b58ba0556a466c6ebbb68dd3806ecf0e8
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-06-14 20:50:58 +0300

    rename id

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,8 @@
 export const refs = {
     //шапка
     header: document.querySelector('.header'),
-    buttonHeaderHome: document.querySelector('#home'),
-    buttonHeaderLibrary: document.querySelector('#library'),
+    buttonHeaderHome: document.querySelector('#button-home'),
+    buttonHeaderLibrary: document.querySelector('#button-library'),
     headerForm: document.querySelector('.header-form'),
     buttonQueue: '',
     buttonWatched: '',
