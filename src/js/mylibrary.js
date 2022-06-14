5a3e7fd8fbecfd88331160c7fe8a68b1efd1be86
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-06-14 17:36:32 +0300

    logic marcup my library

--- src/js/mylibrary.js
+++ src/js/mylibrary.js
@@ -50,10 +50,27 @@ export function renderMyLibraryList() {
   const myLibrary = new MyLibrary(STORAGE_KEY1, STORAGE_KEY2);
 
   if (myLibrary.getMyLibraryId().length) {
-    fechLibraryMovie.query = myLibrary.getMyLibraryId()[0];
-    fechLibraryMovie.fetchFilms();
-    console.log(fechLibraryMovie.fetchFilms());
-    console.log('отрисовываем все фильмы библиотеки');
+    //fechLibraryMovie.query = myLibrary.getMyLibraryId()[0];
+    //fechLibraryMovie.fetchFilms();
+    const idLibraryFilms = myLibrary.getMyLibraryId().join(',');
+    refs.myLibraryContainer.innerHTML = `<span class="user-message"></span>`;
+
+    const message = `There will be movies id ${idLibraryFilms} from the user"s library !!!`;
+
+    const arrMessage = message.split('');
+    let i = 0;
+    const intervalId = setInterval(showLetter, 150);
+
+    function showLetter() {
+      if (i === arrMessage.length) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      refs.myLibraryContainer.firstChild.textContent += arrMessage[i];
+      i += 1;
+    }
+    //console.log('отрисовываем все фильмы библиотеки');
   } else {
     refs.myLibraryContainer.innerHTML = `<span class="user-message"></span>`;
 
