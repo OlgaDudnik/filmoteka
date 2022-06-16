66257db8363c28b4d7dba0be6b5ce974070ea2ec
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-06-17 00:04:45 +0300

    make cards
    update genres and release_date

--- src/js/cards.js
+++ src/js/cards.js
@@ -7,16 +7,48 @@ const refs = {
 
 const fetchMovie = new FetchMovie();
 
-function render() {
-  fetchMovie
-    .fetchPopularFilms()
-    .then(data => {
-      return data.results;
-    })
-    .then(appendArticlesMarkup);
-}
-render();
+loadPopularMovies();
 
-function appendArticlesMarkup(results) {
-  refs.cardColection.insertAdjacentHTML('beforeend', articlesTpl(results));
+async function loadPopularMovies() {
+  const fetchPopMovies = await fetchMovie.fetchPopularFilms();
+  const fetchGenres = await fetchMovie.fetchGenres();
+  const { results } = fetchPopMovies;
+  const { genres } = fetchGenres;
+
+  const movies = createMovies(results, genres);
+  parseMarkup(movies);
+
+  return movies;
+}
+
+function createMovies(returnedFetchMovies, returnedFetchGenres) {
+  return returnedFetchMovies.map(movie => {
+    movie.release_date = movie.release_date
+      ? movie.release_date.split('-')[0]
+      : 'n/a';
+
+    if (movie.genre_ids.length > 0 && movie.genre_ids.length <= 3) {
+      movie.genres = movie.genre_ids
+        .map(id => returnedFetchGenres.filter(el => el.id === id))
+        .flat();
+    }
+
+    if (movie.genre_ids.length > 3) {
+      movie.genres = movie.genre_ids
+        .map(id => returnedFetchGenres.filter(el => el.id === id))
+        .slice(0, 2)
+        .flat()
+        .concat({ name: 'Other' });
+    }
+
+    if (movie.genre_ids.length === 0) {
+      movie.genres = [{ name: 'n/a' }];
+    }
+
+    return movie;
+  });
+}
+
+function parseMarkup(films) {
+  refs.cardColection.insertAdjacentHTML('beforeend', articlesTpl(films));
 }
