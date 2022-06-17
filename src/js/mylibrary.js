8e27af631bc3a5d814d8404d34285ac84d272a42
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-06-18 00:20:23 +0300

    added sort films

--- src/js/mylibrary.js
+++ src/js/mylibrary.js
@@ -11,10 +11,10 @@ const fechLibraryMovie = new FetchMovie();
 
 export default class MyLibrary {
   constructor() {
-    this.watchedKey = [343611, 343611, 136418];
-    this.queueKey = [343611, 973608, 136418, 973608];
-    //this.watchedKey = keys.STORAGE_KEY1;
-    //this.queueKey = keys.STORAGE_KEY2;
+    //this.watchedKey = [343611, 343611, 136418];
+    //this.queueKey = [343611, 973608, 136418, 973608];
+    this.watchedKey = keys.STORAGE_KEY1;
+    this.queueKey = keys.STORAGE_KEY2;
     this.message = 'Your library is empty!!!';
     this.interval = 0;
   }
@@ -29,8 +29,9 @@ export default class MyLibrary {
   }
 
   renderWatchedFilm() {
-    //const queryId = this.getWatchedFilmsId();
-    const queryId = this.watchedKey;
+    const queryId = this.getWatchedFilmsId();
+    queryId.filter(id => id != null).sort((a, b) => a - b);
+    //const queryId = this.watchedKey;
     this.render(queryId);
   }
 
@@ -44,19 +45,18 @@ export default class MyLibrary {
   }
 
   renderQueryFilms() {
-    //const queryId = this.getQueueFilmsId();
-    const queryId = this.queueKey;
+    const queryId = this.getQueueFilmsId();
+    queryId.filter(id => id != null).sort((a, b) => a - b);
+    //const queryId = this.queueKey;
     this.render(queryId);
   }
 
   getLibraryId() {
     const myLibraryId = [];
-    //const watchedFilmsId = this.getWatchedFilmsId();
-    //const queueFilmsId = this.getQueueFilmsId();
-    const watchedFilmsId = this.watchedKey;
-    const queueFilmsId = this.queueKey;
-    console.log(watchedFilmsId);
-    console.log(queueFilmsId);
+    const watchedFilmsId = this.getWatchedFilmsId();
+    const queueFilmsId = this.getQueueFilmsId();
+    //const watchedFilmsId = this.watchedKey;
+    //const queueFilmsId = this.queueKey;
 
     if (watchedFilmsId) {
       myLibraryId.push(...watchedFilmsId);
@@ -64,7 +64,10 @@ export default class MyLibrary {
     if (queueFilmsId) {
       myLibraryId.push(...queueFilmsId);
     }
-    return myLibraryId;
+    return myLibraryId
+      .filter((id, index) => myLibraryId.indexOf(id) === index)
+      .filter(id => id != null)
+      .sort((a, b) => a - b);
   }
 
   renderMyLibrary() {
@@ -74,7 +77,6 @@ export default class MyLibrary {
   }
 
   render(ids) {
-    console.log(ids);
     refs.collection.innerHTML = '';
     if (ids.length) {
       ids.map(id => {
