cdedb46a3b2f8ab11d836c3dac7eb3c781cf42c9
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-06-18 23:06:58 +0300

    add a smooth scroll to the top when switching to another page

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -10,38 +10,47 @@ let page = 1;
 let totalPages = 1000;
 
 async function onPaginationClick(e) {
-    let target = e.target;
-    if (!['SPAN', 'svg', 'path'].includes(target.nodeName)) {
-        return;
-    }
+  let target = e.target;
+  if (!['SPAN', 'svg', 'path'].includes(target.nodeName)) {
+    return;
+  }
 
-    if (['svg', 'path'].includes(target.nodeName)) {
-        target = target.closest('span');
-    }
+  if (['svg', 'path'].includes(target.nodeName)) {
+    target = target.closest('span');
+  }
 
-    if (target.classList.contains('disabled-arrow')) {
-        return;
-    }
+  if (target.classList.contains('disabled-arrow')) {
+    return;
+  }
 
-    if (target.dataset.action === 'left') {
-        FetchMovieInstance.pageNum -= 1;
-        const films = await FetchMovieInstance.fetchPopularFilms();
-        appendArticlesMarkup(films.results);
-        FetchMovieInstance.generatePaginationMarkup(page, totalPages);
-    } else if (target.dataset.action === 'right') {
-        FetchMovieInstance.pageNum += 1;
-        const films = await FetchMovieInstance.fetchPopularFilms();
-        appendArticlesMarkup(films.results);
-        FetchMovieInstance.generatePaginationMarkup(page, totalPages);
-    } else if (target.dataset.action === 'change') {
-        FetchMovieInstance.pageNum = +target.dataset.page;
-        const films = await FetchMovieInstance.fetchPopularFilms();
-        appendArticlesMarkup(films.results);
-        FetchMovieInstance.generatePaginationMarkup(+target.dataset.page, totalPages);
-    }
+  if (target.dataset.action === 'left') {
+    scroll();
+    FetchMovieInstance.pageNum -= 1;
+    const films = await FetchMovieInstance.fetchPopularFilms();
+    appendArticlesMarkup(films.results);
+    FetchMovieInstance.generatePaginationMarkup(page, totalPages);
+  } else if (target.dataset.action === 'right') {
+    scroll();
+    FetchMovieInstance.pageNum += 1;
+    const films = await FetchMovieInstance.fetchPopularFilms();
+    appendArticlesMarkup(films.results);
+    FetchMovieInstance.generatePaginationMarkup(page, totalPages);
+  } else if (target.dataset.action === 'change') {
+    scroll();
+    FetchMovieInstance.pageNum = +target.dataset.page;
+    const films = await FetchMovieInstance.fetchPopularFilms();
+    appendArticlesMarkup(films.results);
+    FetchMovieInstance.generatePaginationMarkup(
+      +target.dataset.page,
+      totalPages
+    );
+  }
+}
+
+function scroll() {
+  window.scrollTo(pageYOffset, 0);
 }
 
 function appendArticlesMarkup(results) {
-    refs.collection.innerHTML = articlesTpl(results);
+  refs.collection.innerHTML = articlesTpl(results);
 }
-
