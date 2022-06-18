778815dee28c4cc900e3f4504491e0663771757a
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-06-18 19:22:15 +0300

    pagination in process

--- src/js/api.fetch.js
+++ src/js/api.fetch.js
@@ -1,7 +1,7 @@
 import renderCard from '../templates/card.hbs';
 import { refs } from './refs.js';
 import { showSpiner, hideSpiner } from './loader';
-import generatePaginationMarkup from './pagination';
+//import generatePaginationMarkup from './pagination';
 
 const LOCALSTORAGE_KEY = 'current-film';
 
@@ -39,6 +39,9 @@ export default class FetchMovie {
     }
 
     set pageNum(newPage) {
+        if (Number(newPage) > 500) {
+            newPage = 500;
+        }
         this.page = newPage;
     }
 
@@ -127,7 +130,61 @@ export default class FetchMovie {
     }
 
     renderPagination() {
-        refs.pagination.innerHTML = generatePaginationMarkup(this.pageNum, this.totalPagesNum);
+        refs.pagination.innerHTML = this.generatePaginationMarkup(this.pageNum, this.totalPagesNum);
+    }
+
+    generatePaginationMarkup(page, totalPages) {
+        if (Number(totalPages) > 500) {
+            totalPages = 500;
+        }
+
+        const backArrow = `<svg class='icon icon-arrow-left' width='16' height='16' viewBox='0 0 16 16' fill='none'
+                     xmlns='http://www.w3.org/2000/svg'>
+                    <path d='M12.6666 8H3.33325' stroke-width='1.33333' stroke-linecap='round'
+                        stroke-linejoin='round' />
+                    <path d='M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337' stroke-width='1.33333'
+                        stroke-linecap='round' stroke-linejoin='round' />
+                    </svg>`;
+        const forwardArrow = `<svg class='icon icon-arrow-right' width='16' height='16' viewBox='0 0 16 16' fill='none'
+                      xmlns='http://www.w3.org/2000/svg'>
+                      <path d='M3.33341 8H12.6667' stroke-width='1.33333' stroke-linecap='round'
+                        stroke-linejoin='round' />
+                      <path d='M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337' stroke-width='1.33333'
+                        stroke-linecap='round' stroke-linejoin='round' />
+                      </svg>`;
+
+        let liItems = '';
+        let activeItem;
+        let beforePages = page - 1;
+        let afterPages = page + 1;
+        let leftDisabledClass = page > 1 ? '' : 'disabled-arrow';
+        let rightDisabledClass = page < totalPages ? '' : 'disabled-arrow';
+        const isNeedToAddDotsBefore = page > 10;
+        const isNeedToAddDotsAfter = page > 10 && page < (totalPages - 1);
+
+        liItems += `<li><span data-action='left' class='pagination__arrow--left ${leftDisabledClass}'>${backArrow}</span></li>`;
+
+        if (isNeedToAddDotsBefore) {
+            liItems += `<li><span data-action='change' data-page='1' class='pagination__button__link'>1</span></li>`;
+            liItems += `<li><span class='pagination__button__dots'>...</span></li>`;
+        }
+
+        for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
+            if (!pageLength || pageLength > totalPages) {
+                continue;
+            }
+            activeItem = page === pageLength ? 'active' : '';
+            liItems += `<li class='${activeItem}'><span data-action='change' data-page='${pageLength}' class='pagination__button__link'>${pageLength}</span></li>`;
+        }
+
+        if (isNeedToAddDotsAfter) {
+            liItems += `<li><span class='pagination__button__dots'>...</span></li>`;
+            liItems += `<li><span data-action='change' data-page='${totalPages}' class='pagination__button__link'>${totalPages}</span></li>`;
+        }
+
+        liItems += `<li><span data-action='right' class='pagination__arrow--right ${rightDisabledClass}'>${forwardArrow}</span></li>`;
+
+        return liItems;
     }
 
     // Получение трейлеров
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,15 +1,15 @@
 import FetchMovie from './api.fetch';
 import { refs } from './refs';
+import articlesTpl from '../templates/card.hbs';
 
-// This line generate error
-//const WHY_THIS_DOES_NOT_WORK = new FetchMovie();
+const FetchMovieInstance = new FetchMovie();
 
 refs.pagination.addEventListener('click', onPaginationClick);
 
 let page = 1;
-let totalPages = 100;
+let totalPages = 1000;
 
-function onPaginationClick(e) {
+async function onPaginationClick(e) {
     let target = e.target;
     if (!['SPAN', 'svg', 'path'].includes(target.nodeName)) {
         return;
@@ -23,66 +23,25 @@ function onPaginationClick(e) {
         return;
     }
 
-    console.log(page, totalPages);
-
     if (target.dataset.action === 'left') {
-        page -= 1;
-        generatePaginationMarkup(totalPages, page);
+        FetchMovieInstance.pageNum -= 1;
+        const films = await FetchMovieInstance.fetchPopularFilms();
+        appendArticlesMarkup(films.results);
+        FetchMovieInstance.generatePaginationMarkup(page, totalPages);
     } else if (target.dataset.action === 'right') {
-        page += 1;
-        generatePaginationMarkup(totalPages, page);
+        FetchMovieInstance.pageNum += 1;
+        const films = await FetchMovieInstance.fetchPopularFilms();
+        appendArticlesMarkup(films.results);
+        FetchMovieInstance.generatePaginationMarkup(page, totalPages);
     } else if (target.dataset.action === 'change') {
-        generatePaginationMarkup(totalPages, +target.dataset.page);
+        FetchMovieInstance.pageNum = +target.dataset.page;
+        const films = await FetchMovieInstance.fetchPopularFilms();
+        appendArticlesMarkup(films.results);
+        FetchMovieInstance.generatePaginationMarkup(+target.dataset.page, totalPages);
     }
 }
 
-export default function generatePaginationMarkup(page, totalPages) {
-    const backArrow = `<svg class='icon icon-arrow-left' width='16' height='16' viewBox='0 0 16 16' fill='none'
-                     xmlns='http://www.w3.org/2000/svg'>
-                    <path d='M12.6666 8H3.33325' stroke-width='1.33333' stroke-linecap='round'
-                        stroke-linejoin='round' />
-                    <path d='M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337' stroke-width='1.33333'
-                        stroke-linecap='round' stroke-linejoin='round' />
-                    </svg>`;
-    const forwardArrow = `<svg class='icon icon-arrow-right' width='16' height='16' viewBox='0 0 16 16' fill='none'
-                      xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M3.33341 8H12.6667' stroke-width='1.33333' stroke-linecap='round'
-                        stroke-linejoin='round' />
-                      <path d='M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337' stroke-width='1.33333'
-                        stroke-linecap='round' stroke-linejoin='round' />
-                      </svg>`;
-
-    let liItems = '';
-    let activeItem;
-    let beforePages = page - 1;
-    let afterPages = page + 1;
-    let leftDisabledClass = page > 1 ? '' : 'disabled-arrow';
-    let rightDisabledClass = page < totalPages ? '' : 'disabled-arrow';
-    const isNeedToAddDotsBefore = page > 10;
-    const isNeedToAddDotsAfter = page > 10 && page < totalPages;
-
-    liItems += `<li><span data-action='left' class='pagination__arrow--left ${leftDisabledClass}'>${backArrow}</span></li>`;
-
-    if (isNeedToAddDotsBefore) {
-        liItems += `<li><span data-action='change' data-page='1' class='pagination__button__link'>1</span></li>`;
-        liItems += `<li><span class='pagination__button__dots'>...</span></li>`;
-    }
-
-    for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
-        if (!pageLength || pageLength > totalPages) {
-            continue;
-        }
-        activeItem = page === pageLength ? 'active' : '';
-        liItems += `<li class='${activeItem}'><span data-action='change' data-page='${pageLength}' class='pagination__button__link'>${pageLength}</span></li>`;
-    }
-
-    if (isNeedToAddDotsAfter) {
-        liItems += `<li><span class='pagination__button__dots'>...</span></li>`;
-        liItems += `<li><span data-action='' data-page='${totalPages}' class='pagination__button__link'>${totalPages}</span></li>`;
-    }
-
-    liItems += `<li><span data-action='right' class='pagination__arrow--right ${rightDisabledClass}'>${forwardArrow}</span></li>`;
-
-    return liItems;
+function appendArticlesMarkup(results) {
+    refs.collection.innerHTML = articlesTpl(results);
 }
 
