cbca13d5fbbeb7527e27b1145f4b065e8170ea4c
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2022-06-21 01:37:55 +0300

    FT-07 update cards.js

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,66 +1,51 @@
 import FetchMovie from './api.fetch';
 import articlesTpl from '../templates/card.hbs';
 import { refs } from './refs';
-// const refs = {
-//     cardColection: document.querySelector('.card__colection'),
-// };
+import data from '../data-base/genres.json';
 
 const fetchMovie = new FetchMovie();
 
 loadPopularMovies();
 
 async function loadPopularMovies() {
-    const fetchPopMovies = await fetchMovie.fetchPopularFilms();
-    // const fetchGenres = await fetchMovie.fetchGenres();
-    const { results } = fetchPopMovies;
-    // const { genres } = fetchGenres;
+  const fetchPopMovies = await fetchMovie.fetchPopularFilms();
+  const { results } = fetchPopMovies;
+  const movies = createMovies(results, data);
+  parseMarkup(movies);
 
-    const movies = createMovies(results);
-    parseMarkup(movies);
-
-    return movies;
+  return movies;
 }
 
-function createMovies(returnedFetchMovies) {
-    return returnedFetchMovies.map(movie => {
-        movie.release_date = movie.release_date ? movie.release_date.split('-')[0] : 'n/a';
+function createMovies(returnedFetchMovies, genresAll) {
+  return returnedFetchMovies.map(movie => {
+    movie.release_date = movie.release_date
+      ? movie.release_date.split('-')[0]
+      : 'n/a';
 
-        // if (movie.genre_ids.length > 0 && movie.genre_ids.length <= 3) {
-        //     movie.genres = movie.genre_ids
-        //         .map(id => returnedFetchGenres.filter(el => el.id === id))
-        //         .flat();
-        // }
+    if (movie.genre_ids.length > 0 && movie.genre_ids.length <= 3) {
+      movie.genres = movie.genre_ids
+        .map(id => genresAll.filter(el => el.id == id))
+        .flat();
+    }
 
-        // if (movie.genre_ids.length > 3) {
-        //     movie.genres = movie.genre_ids
-        //         .map(id => returnedFetchGenres.filter(el => el.id === id))
-        //         .slice(0, 2)
-        //         .flat()
-        //         .concat({ name: 'Other' });
-        // }
+    console.log(returnedFetchMovies);
 
-        // if (movie.genre_ids.length === 0) {
-        //     movie.genres = [{ name: 'n/a' }];
-        // }
+    if (movie.genre_ids.length > 3) {
+      movie.genres = movie.genre_ids
+        .map(id => genresAll.filter(el => el.id == id))
+        .slice(0, 2)
+        .flat()
+        .concat({ name: 'Other' });
+    }
 
-        return movie;
-    });
-}
+    if (movie.genre_ids.length === 0) {
+      movie.genres = [{ name: 'n/a' }];
+    }
 
-function render() {
-    fetchMovie
-        .fetchPopularFilms()
-        .then(data => {
-            return data.results;
-        })
-        .then(appendArticlesMarkup);
+    return movie;
+  });
 }
 
 function parseMarkup(films) {
-    refs.collection.insertAdjacentHTML('beforeend', articlesTpl(films));
+  refs.collection.insertAdjacentHTML('beforeend', articlesTpl(films));
 }
-
-function appendArticlesMarkup(results) {
-    refs.collection.insertAdjacentHTML('beforeend', articlesTpl(results));
-}
-
