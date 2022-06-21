a3ae519144750d55a725ccc0f1ab0f6f3a506266
Kirill Rumiezhak <kirill.rumiezhak@ukr.net>
2022-06-21 13:46:47 +0300

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,27 @@
+export const refs = {
+    //headrer
+    header: document.querySelector('.header'),
+    logoBtn: document.querySelector('.logo'),
+    buttonQueue: '',
+    buttonWatched: '',
+    buttonHeaderHome: document.querySelector('#home'),
+    buttonHeaderLibrary: document.querySelector('#library'),
+    headerForm: document.querySelector('.header-form'),
+    headerListButtons: document.querySelector('.header-list-buttons'),
+
+    //pagination
+    paginationContainer: document.querySelector('.pagination__list'),
+
+    //container
+    collection: document.querySelector('.gallery-container_grid'),
+
+    //modal
+    modalEventListener: document.querySelector('.modalEventListener'),
+    backdrop: document.querySelector('.backdrop'),
+
+    // loader
+    loader: document.querySelector('.loader'),
+
+    // Pagination
+    pagination: document.querySelector('ul.pagination__list'),
+};
